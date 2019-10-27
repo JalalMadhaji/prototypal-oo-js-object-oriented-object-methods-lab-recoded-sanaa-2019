b4f0e064ed588935b222a61b433ba2a42be9789f
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-27 23:07:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function BoardMember(name,homeState,training){
   }
 
   BoardMember.prototype.sayHi = function(){
-    return `Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`;
+    return `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`;
   }
 }
